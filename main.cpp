929c17fdc9be4e739bafa427b1bcacf93e4d7611
Aaron Basch <baschography@gmail.com>
2020-06-03 19:10:49 -0400

    main program, nothing happening yet

---
+++ main.cpp
@@ -0,0 +1,8 @@
+// This program implements linked list as a class
+// multiple functions in separate header file:
+// insert, delete, reverse, print
+
+#include <iostream>
+#include "LinkedList.h"
+
+int main()
